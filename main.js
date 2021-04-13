426cd16ed691aa69b80f96f5f94b38867cf463b7
tunishq <74109308+tunishq@users.noreply.github.com>
2021-04-13 11:08:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/IvM90_bdp/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('model loaded');
+}
